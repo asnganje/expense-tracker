eb24057ffb98c12102d5fbc32a923574725871d1
asnganje <abd.nganje@gmail.com>
2025-05-02 14:24:13 +0300

    add basic components structure

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+const ExpensesSummary = () => {
+  <View>
+    <Text>Last 7 Days</Text>
+    <Text>$177.95</Text>
+  </View>
+}
+
+export default ExpensesSummary;
